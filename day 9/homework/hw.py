687b5a59f5da76de37a3a4aef616fbe829cc0f6c
akako1414 <garretvaginer@gmail.com>
2024-04-07 14:15:21 +0400

    second commit

---
+++ day 9/homework/hw.py
@@ -0,0 +1,7 @@
+salary = int(input(" enter your salary "))
+if salary > 10000:
+    print(" did you study at GOA? ")
+elif salary > 1000 and salary < 10000:
+ print("you mid")
+else:
+ print(" stuady in GOA u in the matrix ")
--- nikoloza/hw.py
+++
@@ -1,6 +0,0 @@
-#unda shevqmna sami cvladi biujeti-300 axali biujeti-300+1000 unda shevqmna cvladi "salary" da biujeti gavyot salaryze da new biujeti\salary movatavso axal cvlat "month"
-biujeti=300
-axali_biujeti=300+1000
-salary=200
-month=axali_biujeti/salary
-print(month)
