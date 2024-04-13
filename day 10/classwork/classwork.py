80f00a4a3dd9453ac1aefcf2f9f6688306f3426c
akako1414 <garretvaginer@gmail.com>
2024-04-13 17:03:41 +0400

    second commit

---
+++ day 10/classwork/classwork.py
@@ -0,0 +1,17 @@
+# age=int(input("enter your age"))
+# if age >= 18:
+#     print("your are a adult")
+
+# else:
+#     print("you are a child")
+
+
+number=int(input("enter a number"))
+if number %2 == 0:
+    print("this number is even")
+
+else:
+    print("this number is prime")
+
+
+
---
+++ day 10/homework/hw.py
@@ -0,0 +1,67 @@
+# მომხმარებელს შეეკითხეთ თუ რომელ პროგრამირების აკადემიაში სწავლობს. თუ პასუხი იქნება "GOA", დაუბეჭდეთ რომ სწორია. სხვა შემთხვევაში დაუბეჭდეთ, რომ არასწორი გადაწყვეტილება მიიღო.
+
+# მომხმარებელს შემოატანინეთ საყიდელი ნივთის ფასი და მისი ბიუჯეტი. თუ ბიუჯეტი აღემატება ან ტოლია საყიდელი ნივთის ფასის, დაუბეჭდეთ რომ თანხა ყოფნის. სხვა შემთხვევაში, დაუბეჭდეთ თუ რამდენი აკლდება საყიდლად.
+
+# დაწერეთ პროგრამა, რომელიც შეამოწმებს მომხმარებლის მიერ შემოტანილ რიცხვს. თუ რიცხვი მეტია ან ტოლი ხუთის, დაბეჭდეთ მისი ნამრავლი ორზე. სხვა შემთხვევაში დაბეჭდეთ მისი ნამრავლი ოთხზე.
+
+# თქვენს პროგრამაში ბილეთის ფასი იქნება 10 ლარი. მომხმარებელს შეეკითხეთ თუ რამდენი ბილეთის ყიდვა უნდა. თუ ეს რიცხვი ნაკლები იქნება 5-ზე, გამოუტანეთ ჩვეულებრივი შედეგი. სხვა შემთხვევაში ყოველ ბილეთზე გააკეთეთ 30%-იანი ფასდაკლება.
+
+# შექმენით კალკულატორის პროგრამა, სადაც გექნებათ +, -, *, /. მომხმარებელს ჯერ შეეკითხეთ ორი რიცხვი, შემდეგ სასურველი ოპერაცია და ბოლოს დაუბეჭდეთ შედეგი
+
+
+# user_academy = (input(" witch programing academy do you go to?: "))
+# if user_academy == ("goa"):
+#       print(" this is correct good job ")
+# else:
+#       print(" you made the wrong choice ")
+     
+
+
+# product_price = int(input(" enter the price of the product "))
+
+# user_budjet = int(input(" enter your budjet "))
+
+# your_money = product_price - user_budjet
+
+# if user_budjet >= product_price:
+#     print(" your budjet is enought you can proceed with your payment ")
+
+# elif user_budjet < product_price:
+#   print(f"you need to have {your_money} more")
+
+
+# number = int(input(" enter a number "))
+# if number >= 5:
+#     print( number * 2)
+# elif number < 5:
+#     print(number * 4)
+
+
+# ticket_price= 10
+# tickets_you_want_to_buy = int(input(" enter the amount of  tickets you want to purchase "))
+# total_price = tickets_you_want_to_buy * ticket_price
+# discount = (ticket_price * tickets_you_want_to_buy) * 0.3
+
+
+# if tickets_you_want_to_buy < 5 :
+#     print(f"the total is {total_price}" )
+# else:
+#     print(f" the total is {total_price - discount } ")
+
+
+
+
+
+# num1 = int(input( "enter a number "))
+# num2 = int(input(" enter second number "))
+# select = input ("enter operation: (*, / , + , - )")
+# if select == "*": 
+#     print(num1 * num2 )
+# elif select == "/":
+#     print(num1 / num2)
+# elif select == "-":
+#     print(num1 - num2)
+# elif select == "+":
+#     print(num1 + num2)
+
+
