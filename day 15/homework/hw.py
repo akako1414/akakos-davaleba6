9c576c4fa89c9fad2033f409a8a94a08f821cbdb
akako1414 <garretvaginer@gmail.com>
2024-04-27 19:15:31 +0400

    second commit

--- day 15/homework/hw.py
+++ day 15/homework/hw.py
@@ -1,6 +1,17 @@
-num = float(input("enter a number"))
-if num < 900:
-    print("bad boy ayo")
+
+score = float(input("შეიყვანეთ თქვენი ტესტის ქულა: "))
+
+
+if score > 90 and score < 100:
+    print("თქვენ დაგიფინანსდებათ სწავლა სრულიად.")
+
+
+elif score > 70 and score < 80:
+    print("თქვენ დაგიფინანსდებათ სწავლა 1500 ლარით.")
+
+
+elif score > 40 and score < 70:
+    print("თქვენ დაგიფინანსდებათ სწავლა 500 ლარით.")
+
 else:
-    print("good boy")
-    
+    print("თქვენ არ დაგიფინანსდებათ სწავლის პროცეს.")
